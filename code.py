9304e56e9055132ac41971a954d3a68baccae347
Ruslan689 <dovhai.pn@ucu.edu.ua>
2023-11-06 16:42:14 +0200

    add docstring

--- code.py
+++ code.py
@@ -1,2 +1,8 @@
 def  validate_board(board: list) -> bool:
+    """
+    this function determines whether the puzzle board is ready for play
+    >>> validate_board(["**** ****","***1 ****","**  3****","* 4 1****",\
+"     9 5 "," 6  83  *","3   1  **","  8  2***","  2  ****"])
+    False
+    """
     pass
